d104152515aaa8a3025b1c2c9ee76fae2308c712
Keith Bossert <Keith.Bossert@verizon.net>
2017-10-18 12:00:27 -0400

    General

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var db = mongojs('writings', ['authors']);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'images')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'authors')));
 
